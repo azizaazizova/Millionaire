11473f6068e063de4b512555b3ae675e9e6b482f
Azizova Aziza <a.azizovaazi@gmail.com>
2025-11-25 19:44:16 +0500

    Initial commit

---
+++ Millionaire/MillionaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionaireApp.swift
+//  Millionaire
+//
+//  Created by Aziza Azizova on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MillionaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
